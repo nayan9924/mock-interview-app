badd96656b66d8b81682551a0d227a2a5fc67a4e
nayan9924 <nayansaggam@gmail.com>
2025-06-13 20:00:15 +0530

    Added Authentication layout And Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
-import PublicLayout  from "./layouts/public-layout";
 
+import PublicLayout  from "@/layouts/public-layout";
+import AuthenticationLayout from "@/layouts/auth-layout";
+
+import HomePage from "@/routes/home";
+import SignIn from "@/routes/sign-in";
+import SignUp from "@/routes/sign-up";
 
 const App = () => {
   return (
@@ -8,8 +13,15 @@ const App = () => {
       <Routes>
         {/*public routes*/}
         <Route element={<PublicLayout />}>
-        
+          <Route index element={<HomePage />}/>
         </Route>
+
+        {/*auth layout*/}
+        <Route element={<AuthenticationLayout/>}>
+          <Route path="/signin" element={<SignIn />}/>
+          <Route path="/signup" element={<SignUp />}/>
+        </Route>
+
         {/*protected routes*/}
       </Routes>
     </Router>
---
+++ src/layouts/auth-layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+const AuthenticationLayout = () => {
+  return (
+    <div className="w-screen h-screen overflow-hidden flex items-center justify-center relative">
+        
+        <Outlet />
+
+    </div>
+  )
+}
+
+export default AuthenticationLayout
